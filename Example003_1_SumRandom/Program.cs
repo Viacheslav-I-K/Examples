636bc3f895b93f1c21341b1264b37a452ec5ac91
Viacheslav <Animal26@yandex.ru>
2022-09-15 16:03:29 +0300

    add Example 003/1 Summ Random Numbers

---
+++ Example003_1_SumRandom/Program.cs
@@ -0,0 +1,9 @@
+﻿int numberA = new Random().Next(1, 10); // Случайное число будет от 1 до 9
+Console.Write("Первое случайное число = ");
+Console.WriteLine(numberA);
+int numberB = new Random().Next(1, 10);
+Console.Write("Второе случайное число = ");
+Console.WriteLine(numberB);
+int result = numberA + numberB;
+Console.Write("Сумма чисел = ");
+Console.WriteLine(result);
