c91dca31e73654f8a443aa2361070b70995ad95a
Viacheslav <Animal26@yandex.ru>
2022-09-21 21:25:55 +0300

    add Example 010 Method Array manual

---
+++ Example010_MethodArray/Program.cs
@@ -0,0 +1,17 @@
+﻿
+int[] array = {11, 12, 31, 47, 18, 53, 16, 17, 18};
+
+int n = array.Length;
+int find = 18;
+int index = 0;
+
+while (index < n)
+{
+    if(array[index] == find)
+    {
+        Console.WriteLine("Индекс искомого числа - " + index);
+        break;
+    }
+    index++;
+}
+
